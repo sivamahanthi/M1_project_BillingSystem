7f32302295d3af572443611acdf30fa90453bd8f
sivamahanthi <102345521+sivamahanthi@users.noreply.github.com>
2022-04-03 20:24:28 +0530

    Add files via upload

---
+++ 3_Implementation/Src/Quality.c
@@ -0,0 +1,26 @@
+#include "bill.h"
+
+void quantity()
+{
+    printf("\n\t***Enter 0 for none****");
+
+	printf("\n No. of Teas \n");
+	scanf("%d", &teaQTY);
+
+	printf(" No. of SpeacialTea \n");
+	scanf("%d", &specialteaQTY);
+
+	printf(" No. of coffees\n");
+	scanf("%d", &coffeeQTY);
+
+	printf(" No. of buscuits\n");
+	scanf("%d", &buscuitQTY);
+
+	printf(" No. of buns \n");
+	scanf("%d", &bunQTY);
+
+	printf(" No. of Milks \n");
+	scanf("%d", &milkQTY);
+	
+billgeneration();
+}
---
+++ 3_Implementation/Src/logic.c
@@ -0,0 +1,14 @@
+#include "bill.h"
+
+void login()
+  { 
+    printf("administrator: \n");
+	scanf("%s",name);
+
+	printf("Customer's Name: \n");
+	scanf("%s",cname);
+
+	printf("Date (dd/mm/yyyy)\n");
+	scanf("%s",date);
+quantity();
+}
