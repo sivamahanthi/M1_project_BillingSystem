32aacea7d5162cbcc48358f06832874380bdb7d2
sivamahanthi <102345521+sivamahanthi@users.noreply.github.com>
2022-04-03 20:25:32 +0530

    Add files via upload

---
+++ 3_Implementation/Test/Test.c
@@ -0,0 +1,29 @@
+#include "bill.h"
+#include "unity.h"
+
+void setUp()
+{
+
+}
+
+void tearDown()
+{
+
+}
+
+void test_login (void);
+
+
+int main()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(login);
+
+    return UNITY_END();
+}
+
+void test_login (void)
+{   
+    UNITY_TEST_ASSERT_EQUAL_INT((-1), (login()), __LINE__, NULL);
+}
